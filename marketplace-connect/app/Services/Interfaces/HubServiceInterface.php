e4307737c5911775bbb44f8af48fbb6e96a1c029
HenriqueCCicero <henrique.c.cicero@hotmail.com>
2025-03-21 18:15:27 -0300

    docs: Melhorar documentação do HUB

--- marketplace-connect/app/Services/Interfaces/HubServiceInterface.php
+++ marketplace-connect/app/Services/Interfaces/HubServiceInterface.php
@@ -7,7 +7,12 @@ use App\Entities\OfferHubEntity;
 interface HubServiceInterface
 {
     /**
-     * Get offers.
+    * Creates a new offer in the hub.
+    *
+    * This method sends a request to the hub service to create an offer
+    * using the provided OfferHubEntity.
+    *
+    * @param OfferHubEntity $entity The offer entity to be created in the hub.
      */
     public function createOffer(OfferHubEntity $entity): void;
 }
