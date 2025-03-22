061de74202bb6b29af62b51fc5249cecdff9837b
HenriqueCCicero <henrique.c.cicero@hotmail.com>
2025-03-22 14:45:56 -0300

    style: Ajustar formatação de comentários e espaços em branco

--- app/Repositories/Interfaces/OfferPriceRepositoryInterface.php
+++ app/Repositories/Interfaces/OfferPriceRepositoryInterface.php
@@ -7,9 +7,9 @@ use App\Entities\OfferPriceEntity;
 interface OfferPriceRepositoryInterface
 {
     /**
-    * Salva a entidade de preço da oferta no banco de dados.
-    *
-    * Este método cria um novo registro para o preço da oferta.
+     * Salva a entidade de preço da oferta no banco de dados.
+     *
+     * Este método cria um novo registro para o preço da oferta.
      *
      * @param  OfferPriceEntity  $entity  A entidade de preço da oferta a ser persistida.
      */
